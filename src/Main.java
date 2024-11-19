dc7dfafd3098abd8e859256120fa603683c36ef6
alexlegreat <yabrofaj@mail.uc.edu>
2024-11-18 22:37:01 -0500

    completed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>(); 
+        ArrayList<String> list = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         boolean running = true;
 
